fe7add970b284867f969d1184f2cb276121b8543
yenting_liu <s2223695@yahoo.com.tw>
2019-11-28 16:21:12 +0800

    Add relations between User and Record models

--- models/record.js
+++ models/record.js
@@ -29,7 +29,7 @@ const recordSchema = new Schema({
     type: Schema.Types.ObjectId,
     ref: 'User',
     index: true,
-    // required: true
+    required: true
   }
 })
 
--- routes/record.js
+++ routes/record.js
@@ -7,10 +7,12 @@ const { authenticated } = require('../config/auth')
 const Record = require('../models/record').Record
 
 route.get('/', authenticated, (req, res) => {
-  Record.find((err, records) => {
-    if (err) console.error(err)
-    res.render('index', { records })
-  })
+  Record.find({ userId: req.user._id })
+    .sort({ name: 'asc' })
+    .exec((err, records) => {
+      if (err) console.error(err)
+      res.render('index', { records })
+    })
 })
 
 route.get('/new', authenticated, (req, res) => {
@@ -24,7 +26,8 @@ route.post('/new', authenticated, (req, res) => {
     category: req.body.category,
     date: req.body.date,
     amount: req.body.amount,
-    totalAmount: req.body.amount
+    totalAmount: req.body.amount,
+    userId: req.user._id
   })
   record.save(err => {
     if (err) return console.error(err)
@@ -33,20 +36,21 @@ route.post('/new', authenticated, (req, res) => {
 })
 
 route.get('/:id/edit', authenticated, (req, res) => {
-  Record.findById(req.params.id, (err, record) => {
+  Record.findOne({ _id: req.params.id, userId: req.user._id }, (err, record) => {
     if (err) return console.error(err)
     res.render('edit', { record })
   })
 })
 
 route.put('/:id', authenticated, (req, res) => {
-  Record.findById(req.params.id, (err, record) => {
+  Record.findOne({ _id: req.params.id, userId: req.user._id }, (err, record) => {
     if (err) return console.error(err)
     record.name = req.body.name
     record.date = req.body.date
     record.category = req.body.category
     record.amount = req.body.amount
     record.totalAmount = req.body.amount
+    record.userId = req.user._id
     record.save(err => {
       if (err) return console.error(err)
       res.redirect('/')
@@ -55,7 +59,7 @@ route.put('/:id', authenticated, (req, res) => {
 })
 
 route.delete('/:id/delete', authenticated, (req, res) => {
-  Record.findByIdAndDelete(req.params.id, (err, record) => {
+  Record.findOneAndDelete({ _id: req.params.id, userId: req.user._id }, (err, record) => {
     if (err) return console.error(err)
     res.redirect('/')
   })
